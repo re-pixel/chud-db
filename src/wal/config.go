65753b6f40286bffa357de23f062bbb0b8affb34
re-pixel <relja.brdar@gmail.com>
2026-06-12 18:38:06 +0200

    feat: add wal-specific config

---
+++ src/wal/config.go
@@ -0,0 +1,23 @@
+package wal
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+)
+
+var walConfigData []byte
+
+type WalConfig struct {
+	WALDir              string `json:"WAL_DIR"`
+	WALSegmentSize      int64  `json:"WAL_SEGMENT_SIZE"`
+	WALWriteBufferSize  int    `json:"WAL_WRITE_BUFFER_SIZE"`
+}
+
+func GetWalConfig() WalConfig {
+	var cfg WalConfig
+	if err := json.Unmarshal(walConfigData, &cfg); err != nil {
+		panic(fmt.Sprintf("failed to parse wal config file: %v", err))
+	}
+	return cfg
+}
